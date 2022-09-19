7225d9dded58214439c8437086c36abd7f4b5505
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-09-19 09:52:11 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17972 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        double A,B;
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+        System.out.println(A / B);
+        
+        
+        
+    }
+}

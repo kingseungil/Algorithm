4ce17f2ec991577f3cd20144d3e901b084d64866
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-09-21 12:05:09 +0900

    Update 올바른 괄호.java

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,5 +1,6 @@
 import java.util.Stack;
 
+// stack사용안한 풀이
 class Solution {
        boolean solution(String s){
         boolean answer = false;
@@ -18,6 +19,30 @@ class Solution {
         }
         // count = 0 이라는건 쌍이 맞춰졌다는 뜻
         if (count == 0){
+            answer = true;
+        }
+        return answer;
+    }
+}
+
+// stack사용한 풀이
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s){
+        boolean answer = false;
+        Stack stack = new Stack();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a'){
+                stack.push(s.charAt(i));
+            } else {
+                if (stack.isEmpty()){
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        if (stack.isEmpty()){
             answer = true;
         }
         return answer;

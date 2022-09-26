cd44a072d52be29496624ea007e630ecf91ba741
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-09-26 14:41:06 +0900

    Update ［1차］ 비밀지도.java

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,5 +1,17 @@
 class Solution {
    public String[] solution(int n, int[] arr1, int[] arr2) {
+       String[] answer = new String[n];
+       String[] answer = new String[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = String.format("%" + n + "s", Integer.toBinaryString(arr1[i] | arr2[i]));
+            answer[i] = answer[i].replaceAll("1", "#").replaceAll("0"," ");
+        }
+      return answer;
+   }
+}
+      
+      // 아래처럼 바꾸면 성능이 훨씬 향상됨
+      /*
         String[] answer = new String[n];
         String temp;
         for (int i = 0; i < n; i++) {
@@ -10,5 +22,6 @@ class Solution {
         }
         return answer;
     }
+    */
 
 }

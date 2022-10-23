9877fa16db2216fb5e9b9324e51790bf0b046e0b
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-10-23 11:01:57 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 232 ms, Memory: 18344 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args){
+        int[] result = {1, 1, 2, 2, 2, 8};
+        int[] input = new int[6];
+        int[] output = new int[6];
+        // 입력부분
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < input.length; i++) {
+            input[i] = sc.nextInt();
+            if (input[i] != result[i]) {
+                output[i] = result[i] - input[i];
+            } else {
+                output[i] = 0;
+            }
+        }
+        for (int i = 0; i < output.length; i++) {
+            System.out.print(output[i] + " ");
+        }
+    }
+}

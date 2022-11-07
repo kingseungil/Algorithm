2dea4ec663ab9132083966e62f361560c557a307
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-11-07 11:15:54 +0900

    Create linearSearch.js

---
+++ JavaScript알고리즘 & 자료구조 마스터클래스/검색 알고리즘/선형 검색/linearSearch.js
@@ -0,0 +1,11 @@
+// Big O -> O(N)
+function linearSearch(arr,val) {
+  for (let i = 0; i <arr.length; i++) {
+    if (arr[i] ===val) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(linearSearch([10, 15, 20, 25, 30], 25));

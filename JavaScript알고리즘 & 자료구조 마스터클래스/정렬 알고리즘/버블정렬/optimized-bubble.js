96a79eb8a9ad9f01277a6572c7639a3cb9eef1aa
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-11-08 10:31:08 +0900

    Update optimized-bubble.js

--- JavaScript알고리즘 & 자료구조 마스터클래스/정렬 알고리즘/버블정렬/optimized-bubble.js
+++ JavaScript알고리즘 & 자료구조 마스터클래스/정렬 알고리즘/버블정렬/optimized-bubble.js
@@ -9,13 +9,13 @@ function bubbleSort(arr) {
   for (let i = arr.length; i > 0; i--) {
     noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
-			console.log(arr,arr[j],arr[j + 1]);
+      console.log(arr,arr[j],arr[j + 1]);
       if (arr[j] >arr[j + 1]) {
-				// SWAP!
-				let temp =arr[j];
-				arr[j] =arr[j + 1];
-				arr[j + 1] = temp;
-				noSwaps = false;
+        // SWAP!
+	let temp =arr[j];
+	arr[j] =arr[j + 1];
+	arr[j + 1] = temp;
+	noSwaps = false;
       }
     }
     if (noSwaps) break;

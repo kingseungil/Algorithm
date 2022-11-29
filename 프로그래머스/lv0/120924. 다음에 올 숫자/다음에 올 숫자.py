184aec40a072ef88e9c8df75bbd5faa3e0b74447
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-11-29 12:32:43 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,13 +1,5 @@
 def solution(common):
-    c = common
-    answer = 0
-    # 등비중항으로 판별
-    for i in range(0,len(common)):
-        if c[i+1] ** 2 == c[i] * c[i+2] and c[i]:    
-            answer = c[-1] * (c[i+1] / c[i])
-        else:
-             answer = c[-1] + (c[i+1] - c[i])
-        return answer
-    
-    
-    return answer
+    isSequence = common[2] - common[1] == common[1] - common[0]
+    if isSequence:
+        return common[-1] + common[1] - common[0]
+    return common[-1] * (common[1] / common[0])

bc7d3fae20496f94b6531e35e489ae564e53c189
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-12-01 21:18:24 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,11 +1,13 @@
 def solution(my_string, letter):
-    str_to_list = list(my_string)
-    list2 = list(my_string)
+    return my_string.replace(letter,'')
+#     str_to_list = list(my_string)
+#     list2 = list(my_string)
     
-    for i in list2:
-        if i == letter:
-            str_to_list.remove(letter)
+#     for i in list2:
+#         if i == letter:
+#             str_to_list.remove(letter)
             
-    list_to_str = ''.join(map(str,str_to_list))
+#     list_to_str = ''.join(map(str,str_to_list))
     
-    return list_to_str
+#     return list_to_str
+       

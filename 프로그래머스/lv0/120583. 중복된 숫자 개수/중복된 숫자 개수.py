7715d61b1b370f34b22756be2e2b178e16cc3caf
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-12-01 17:55:47 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
@@ -1,6 +1,7 @@
 def solution(array, n):
-    count = 0
-    for i in array:
-        if n == i:
-            count += 1
-    return count
+    # count = 0
+    # for i in array:
+    #     if n == i:
+    #         count += 1
+    # return count
+    return array.count(n)

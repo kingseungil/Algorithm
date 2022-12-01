763e22b7368f48c8d42abac5e88d65e9a0d5dc95
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-12-01 19:25:23 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.py
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.py
@@ -1,5 +1,6 @@
 def solution(strlist):
-    answer = []
-    for i in strlist:
-        answer.append(len(i))
-    return answer
+    # answer = []
+    # for i in strlist:
+    #     answer.append(len(i))
+    # return answer
+    return [len(i) for i in strlist]

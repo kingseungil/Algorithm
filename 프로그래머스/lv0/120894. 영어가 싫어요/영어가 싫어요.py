3cbbf3d9d3f1777732ebd46277b4563d87fc6f17
kingseungil <109774037+kingseungil@users.noreply.github.com>
2022-12-16 12:33:06 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,10 +1,13 @@
 def solution(numbers):
-    num = ["zero", "one", "two", "three", "four",
-       "five", "six", "seven", "eight", "nine"]
+#     num = ["zero", "one", "two", "three", "four",
+#        "five", "six", "seven", "eight", "nine"]
 
-    num2 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+#     num2 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
-    result = numbers.replace(num[0], num2[0]).replace(num[1], num2[1]).replace(num[2], num2[2]).replace(num[3], num2[3]).replace(num[4], num2[4]).replace(num[5], num2[5]).replace(num[6], num2[6]).replace(num[7], num2[7]).replace(num[8], num2[8]).replace(num[9], num2[9])
+#     result = numbers.replace(num[0], num2[0]).replace(num[1], num2[1]).replace(num[2], num2[2]).replace(num[3], num2[3]).replace(num[4], num2[4]).replace(num[5], num2[5]).replace(num[6], num2[6]).replace(num[7], num2[7]).replace(num[8], num2[8]).replace(num[9], num2[9])
+    for i, j in enumerate(["zero", "one", "two", "three", "four",
+                       "five", "six", "seven", "eight", "nine"]):
+        numbers = numbers.replace(j, str(i))
     
-    return int(result)
+    return int(numbers)

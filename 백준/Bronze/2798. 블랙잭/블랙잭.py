6b45f192d393225a8c6b1799f99e1f900371ad10
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-01-19 12:01:46 +0900

    [Bronze II] Title: 블랙잭, Time: 108 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,16 @@
+import sys
+N,M = list(map(int, sys.stdin.readline().split(' ')))
+data = list(map(int, sys.stdin.readline().split(' ')))
+
+result = 0
+length = len(data)
+for i in range(0, length):
+    for j in range(i+1, length):
+        for k in range(j+1, length):
+            sum = data[i]+data[j]+data[k]
+            if sum <= M:
+                result = max(result, sum)
+
+print(result)
+        
+    

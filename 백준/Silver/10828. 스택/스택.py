080e2d723aff945c268ecf172c3ce1c46bb2d27b
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-01-31 18:19:33 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,25 @@
+import sys
+N = int(input())
+stack = []
+
+for _ in range(N):
+    command = sys.stdin.readline().strip().split(' ')
+    if command[0] == 'push':
+        stack.append(int(command[1]))
+    elif command[0] == 'pop':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'top':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])

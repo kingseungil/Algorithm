3b4e2b0a5ed808e8ed7376c95b7329a02542769d
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-04-05 18:31:59 +0900

    [Bronze V] Title: 합, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.js
+++ 백준/Bronze/8393. 합/합.js
@@ -5,7 +5,7 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 // 풀이
 function solution(arr) {
   const a = +arr[0]
-  let sum = a * (a+1) / 2
+  const sum = a * (a+1) / 2
   return sum
 }
 

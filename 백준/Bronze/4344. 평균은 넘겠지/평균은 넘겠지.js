6f04ff441faafc930fa7f513025c4a8eb1acf35c
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-04-07 15:34:41 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 132 ms, Memory: 10912 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.js
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.js
@@ -3,10 +3,10 @@ let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
 const testCase = +input[0]
 for (let i = 1; i <= testCase; i++) {
-  let data = input[i].split(' ').map(Number)
-  let [b, ...c] = data
-  let sum = c.reduce((a, b) => a + b)
-  let avg = sum / b
+  const data = input[i].split(' ').map(Number)
+  const [b, ...c] = data
+  const sum = c.reduce((a, b) => a + b)
+  const avg = sum / b
   let pass = 0
   for (let j = 0; j < c.length; j++) {
     if (c[j] > avg) {
@@ -16,4 +16,5 @@ for (let i = 1; i <= testCase; i++) {
   const result = pass / b * 100
   console.log(result.toFixed(3) + '%');
 }
+
 

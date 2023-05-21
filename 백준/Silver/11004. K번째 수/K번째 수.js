52a1e21e318defb3f8aa7b54bbedd53af6bf5e05
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-05-21 21:25:18 +0900

    [Silver V] Title: K번째 수, Time: 4516 ms, Memory: 564788 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.js
+++ 백준/Silver/11004. K번째 수/K번째 수.js
@@ -2,9 +2,8 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 // const input = fs.readFileSync('test.txt').toString().trim().split('\n');
 
-const N = +input[0].split(' ')[0];
-const K = +input[0].split(' ')[1];
-const arr = input[1].split(' ').map((x) => +x);
-
+const [_N, K] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
 arr.sort((a, b) => a - b);
+
 console.log(arr[K - 1]);

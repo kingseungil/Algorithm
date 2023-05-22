600c6674a5b6dbfcc03751f7d9be11bf48957750
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-05-22 11:45:23 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 496 ms, Memory: 59080 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// const input = fs.readFileSync('test.txt').toString().trim().split('\n');
+
+const N = parseInt(input[0], 10);
+
+let arr = [];
+for (let i = 1; i <= N; i++) {
+  const [x, y] = input[i].split(' ').map(Number);
+  arr.push([x, y]);
+}
+
+arr.sort((a, b) => a[0] - b[0] || a[1] - b[1]);
+
+let answer = '';
+for (let i = 0; i < N; i++) {
+  answer += arr[i][0] + ' ' + arr[i][1] + '\n';
+}
+console.log(answer);

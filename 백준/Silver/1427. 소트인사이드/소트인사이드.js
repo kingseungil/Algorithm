9ddce1f1528b89f2b753f0c8798b58e3edd86cd2
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-05-22 17:55:51 +0900

    [Silver V] Title: 소트인사이드, Time: 120 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.js
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -4,9 +4,17 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const N = input[0];
 
-let arr = [];
-for (let i = 0; i < N.length; i++) {
-  arr.push(N[i]);
+let cnt = Array(10).fill(0);
+
+for (let x of N) {
+  cnt[Number(x)]++;
 }
-arr.sort((a, b) => parseInt(b) - parseInt(a));
-console.log(arr.join(''));
+
+let answer = '';
+for (let i = 9; i >= 0; i--) {
+  for (let j = 0; j < cnt[i]; j++) {
+    answer += i + '';
+  }
+}
+
+console.log(answer);

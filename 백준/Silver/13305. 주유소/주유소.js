8312da3c5aa8bedce5b431fc82d68b69d41f25bb
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-06-05 22:28:00 +0900

    [Silver III] Title: 주유소, Time: 208 ms, Memory: 11344 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,20 +1,22 @@
 const fs = require('fs');
-const filePath =
-  process.platform === 'linux' ? '/dev/stdin' : __dirname + '/test.txt';
+const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/test.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
-
-const n = +input[0];
 const distances = input[1].split(' ').map(Number);
 const prices = input[2].split(' ').map(Number);
 
-let minPrice = prices[0];
-for (let i = 0; i < n; i++) {
-  minPrice = Math.min(minPrice, prices[i]);
-  prices[i] = minPrice;
-}
+// 각 도시의 기름값을 최소 기름값으로 업데이트
+const updatedPrices = prices.reduce((acc, price, i) => {
+  const minPrice = i === 0 ? price : Math.min(price, acc[i - 1]);
+  return [...acc, minPrice];
+}, []);
 
-let cost = BigInt(0);
-for (let i = 0; i < n - 1; i++) {
-  cost += BigInt(distances[i]) * BigInt(prices[i]);
-}
-console.log(String(cost));
+// 거리 * 기름값을 BigInt로 계산
+const calculateCost = (distances, prices) => {
+  return distances.reduce((acc, distance, i) => {
+    const currentCost = BigInt(distance) * BigInt(prices[i]);
+    return acc + currentCost;
+  }, BigInt(0));
+};
+
+const cost = calculateCost(distances, updatedPrices);
+console.log(String(cost)); // BigInt는 뒤에 n이 붙으므로 String으로 변환

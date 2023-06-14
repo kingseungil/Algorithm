ee73c347f1860ae7afb2d074572188e933818db3
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-06-14 12:59:31 +0900

    [Gold V] Title: 회문, Time: 276 ms, Memory: 26884 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.js
+++ 백준/Gold/17609. 회문/회문.js
@@ -2,36 +2,43 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/test.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
+// 회문인지 검사하는 함수
 const isPalindrome = (input) => {
-  // 회문인지 검사
-  const reverseInput = input.split('').reverse().join('');
-  if (input === reverseInput) {
-    return true;
+  const n = input.length;
+  for (let i = 0; i < n / 2; i++) {
+    if (input[i] !== input[n - i - 1]) {
+      return false;
+    }
   }
+  return true;
 };
 
 const t = +input[0];
+
 for (let i = 1; i <= t; i++) {
-  let data = input[i];
+  const data = input[i];
   // 회문인 경우
-  if (isPalindrome(data)) console.log(0);
-  // 유사회문인지 검사
+  if (isPalindrome(data)) {
+    console.log(0);
+  }
+  // 유사 회문인 경우
   else {
-    let flag = false; // 유사 회문 여부 확인
-    let n = +data.length;
-    for (let j = 0; j < n / 2; j++) {
+    let flag = false; // 유사 회문인지 아닌지 판별하는 변수
+    const n = data.length;
+    for (let j = 0; j < n / 2 && !flag; j++) {
       // 대칭이 아닌 경우
       if (data[j] !== data[n - j - 1]) {
-        // 왼쪽에 있는 원소를 제거한 뒤 회문 검사
-        if (isPalindrome(data.slice(0, j) + data.slice(j + 1, n))) flag = true;
-        // 오른쪽에 있는 원소를 제거한 뒤 회문 검사
+        // 왼쪽에 있는 원소를 제거한 뒤, 회문인지 검사
+        if (isPalindrome(data.slice(0, j) + data.slice(j + 1))) flag = true;
+        // 오른쪽에 있는 원소를 제거한 뒤, 회문인지 검사
         if (isPalindrome(data.slice(0, n - j - 1) + data.slice(n - j))) flag = true;
         break;
       }
     }
-    // 유사 회문인 경우
-    if (flag) console.log(1);
-    // 그 외
-    else console.log(2);
+    if (flag) {
+      console.log(1);
+    } else {
+      console.log(2);
+    }
   }
 }

3797eda75644d921c8c71f4d514a25354927d642
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-06-20 13:55:00 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1136 ms, Memory: 140000 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -8,24 +8,43 @@ const input = fs.readFileSync(filePath).toString().trim().split('\n');
 const numberArr = input[1].split(' ').map(Number); // 숫자 카드에 적혀있는 정수
 const targetArr = input[3].split(' ').map(Number); // 몇 개 가지고 있는 숫자 카드인지 구해야 할 정수
 
-const binarySearch = (arr, targetArr) => {
-  const result = Array.from({ length: targetArr.length }, () => 0);
-  const numCnt = {};
+const lowerBound = (arr, target) => {
+  let start = 0,
+    end = arr.length;
 
-  // 각 숫자 카드 종류별로 개수를 구합니다.
-  for (const num of arr) {
-    numCnt[num] = (numCnt[num] || 0) + 1;
+  while (start < end) {
+    const mid = Math.floor((start + end) / 2);
+    if (arr[mid] >= target) end = mid;
+    else start = mid + 1;
   }
 
-  // 숫자 카드 종류별로 개수를 조회한 결과를 반환합니다.
+  return end;
+};
+
+const upperBound = (arr, target) => {
+  let start = 0,
+    end = arr.length;
+
+  while (start < end) {
+    const mid = Math.floor((start + end) / 2);
+    if (arr[mid] > target) end = mid;
+    else start = mid + 1;
+  }
+
+  return end;
+};
+
+const countNums = (numArr, targetArr) => {
+  const result = Array.from({ length: targetArr.length }, () => 0);
+  numArr.sort((a, b) => a - b);
+
   for (let i = 0; i < targetArr.length; i++) {
-    const target = targetArr[i];
-    if (numCnt.hasOwnProperty(target)) {
-      result[i] = numCnt[target];
-    }
+    const leftIdx = lowerBound(numArr, targetArr[i]);
+    const rightIdx = upperBound(numArr, targetArr[i]);
+    result[i] = rightIdx - leftIdx;
   }
 
   return result;
 };
 
-console.log(binarySearch(numberArr, targetArr).join(' '));
+console.log(countNums(numberArr, targetArr).join(' '));

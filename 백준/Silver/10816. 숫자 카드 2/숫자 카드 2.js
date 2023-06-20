b48b4669f78bacae90fb9562307d99d884d40107
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-06-20 14:07:11 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1084 ms, Memory: 124096 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -5,38 +5,43 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/test.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-const numberArr = input[1].split(' ').map(Number); // 숫자 카드에 적혀있는 정수
+const numberArr = input[1]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b); // 숫자 카드에 적혀있는 정수
 const targetArr = input[3].split(' ').map(Number); // 몇 개 가지고 있는 숫자 카드인지 구해야 할 정수
 
-const lowerBound = (arr, target) => {
+// lowerBound : target보다 크거나 같은 수가 처음으로 나타나는 위치를 반환하는 함수
+const lowerBound = (numArr, target) => {
   let start = 0,
-    end = arr.length;
+    end = numArr.length;
 
   while (start < end) {
     const mid = Math.floor((start + end) / 2);
-    if (arr[mid] >= target) end = mid;
+    if (numArr[mid] >= target) end = mid;
     else start = mid + 1;
   }
 
   return end;
 };
 
-const upperBound = (arr, target) => {
+// upperBound : target보다 큰 수가 처음으로 나타나는 위치를 반환하는 함수
+const upperBound = (numArr, target) => {
   let start = 0,
-    end = arr.length;
+    end = numArr.length;
 
   while (start < end) {
     const mid = Math.floor((start + end) / 2);
-    if (arr[mid] > target) end = mid;
+    if (numArr[mid] > target) end = mid;
     else start = mid + 1;
   }
 
   return end;
 };
 
+// countNums : lowerBound와 upperBound를 이용하여 targetArr에 해당하는 숫자 카드의 개수를 구하는 함수
 const countNums = (numArr, targetArr) => {
   const result = Array.from({ length: targetArr.length }, () => 0);
-  numArr.sort((a, b) => a - b);
 
   for (let i = 0; i < targetArr.length; i++) {
     const leftIdx = lowerBound(numArr, targetArr[i]);

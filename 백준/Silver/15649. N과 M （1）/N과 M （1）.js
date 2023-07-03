359ce35b60b1cf93fbc1e2ccd547b69067659e2b
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-07-03 18:27:53 +0900

    [Silver III] Title: N과 M (1), Time: 224 ms, Memory: 17372 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -1,29 +1,31 @@
+/**
+ * https://www.acmicpc.net/problem/15650 (N과 M (2))
+ */
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/test.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const [N, M] = input[0].split(' ').map(Number);
 
-let result = [];  // 백트래킹 결과를 저장할 배열
+let result = [];
+let arr = Array.from({ length: M }, () => 0);
+let visited = Array.from({ length: M }, () => false);
 
-// depth-현재 탐색 깊이, N-최대 범위, M-탐색해야 할 깊이
 const dfs = (depth, N, M) => {
-  // 탐색 깊이가 M과 같아지면 탐색 종료
   if (depth === M) {
-    console.log(result.join(' '));  // 결과 출력, 탐색한 수들을 공백으로 연결해서 출력
+    result += arr.join(' ') + '\n';
     return;
   }
 
-  // 브루트포스 탐색: 1부터 N까지 모든 수를 탐색
   for (let i = 1; i <= N; i++) {
-    // 이미 탐색한 수라면 건너뛰고 다음 반복
-    if (result.includes(i)) continue;
-    
-    // 백트래킹 수행: 현재 탐색하려는 수를 결과 배열에 추가
-    result.push(i);
-    dfs(depth + 1, N, M); // 깊이를 늘려서 다음 수를 탐색
-    result.pop(); // 백트래킹:  다음 수를 탐색하기 전에 마지막으로 탐색한 수를 제거
+    if (visited[i]) continue;
+
+    arr[depth] = i;
+    visited[i] = true;
+    dfs(depth + 1, N, M);
+    visited[i] = false;
   }
 };
 
-dfs(0, N, M);  // 탐색 시작
+dfs(0, N, M);
+console.log(result);

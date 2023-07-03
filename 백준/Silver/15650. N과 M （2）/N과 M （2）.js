45d05eaffe3d8ba28cf59d035722fc4de077070d
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-07-03 18:46:00 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.js
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -7,29 +7,25 @@ const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const [N, M] = input[0].split(' ').map(Number);
 
-let result = [];
+let result = '';
 let arr = Array.from({ length: M }, () => 0);
-let visited = Array.from({ length: M }, () => false);
-let startNum = Array.from({ length: M }, () => 1);
+let startNum = Array.from({ length: M }, (_, i) => i + 1); 
 
 const dfs = (depth, N, M) => {
-  if (depth === M) {
+  if (depth === M) {  
     result += arr.join(' ') + '\n';
     return;
   }
 
   for (let i = startNum[depth]; i <= N; i++) {
-    if (visited[i]) continue;
-
     arr[depth] = i;
-    visited[i] = true;
 
+    // depth가 M - 1이 아니라면, 다음 탐색할 시작 숫자 업데이트
     if (depth !== M - 1) {
       startNum[depth + 1] = i + 1;
     }
 
     dfs(depth + 1, N, M);
-    visited[i] = false;
   }
 };
 

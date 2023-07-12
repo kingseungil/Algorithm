dc059062b6012666048e7092e82fb06dc85982b8
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-07-12 15:35:08 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -7,12 +7,13 @@ class Solution {
         }
         // 두 숫자는 같고 나머지 하나는 다르면
         if ((a == b && b != c) || (b == c && c != a) || (c == a && a != b)) {
-          answer = (a + b + c) * ((a * a) + (b * b) + (c * c));
+          answer = (int) ((a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)));
         }
         // 모두 같으면
         if (a == b && b == c) {
           answer =
-              (a + b + c) * ((a * a) + (b * b) + (c * c)) * ((a * a * a) + (b * b * b) + (c * c * c));
+              (int) ((a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)) * (Math.pow(a, 3)
+                  + Math.pow(b, 3) + Math.pow(c, 3)));
         }
         
         return answer;

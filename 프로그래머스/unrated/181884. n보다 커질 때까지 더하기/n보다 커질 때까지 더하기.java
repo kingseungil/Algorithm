f0deaf22001728ce09b778ec6b0b65abf30c2c67
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-07-25 15:16:15 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int sum = 0;
+        for (int num : numbers){
+            if(sum > n) break;
+            sum += num;
+        }
+        
+        return sum;
+    }
+}

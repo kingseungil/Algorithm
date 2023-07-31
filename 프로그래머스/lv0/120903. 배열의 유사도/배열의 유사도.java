81d63f54267cd53511a1b3ffaad836197552d171
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-07-31 20:55:12 +0900

    [level 0] Title: 배열의 유사도, Time: 0.07 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        
+        for (String str : s1){
+            if(Arrays.asList(s2).contains(str)){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}

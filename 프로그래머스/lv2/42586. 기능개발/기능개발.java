863b6c2418e2f7e87cd85422af0c0cd174a8f952
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-08-09 10:46:48 +0900

    [level 2] Title: 기능개발, Time: 2.11 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -2,25 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        ArrayList<Integer> answerList = new ArrayList<>();
-
-    int days = 0; // 현재 필요한 최대 작업일 수
-    int cnt = 0; // 같은 날에 배포되는 기능의 수를 세는 변수
+    Queue<Integer> queue = new ArrayDeque<>(); // 각 기능의 남은 날짜를 저장하기 위한 큐
+    ArrayList<Integer> answerList = new ArrayList<>();
 
     for (int i = 0; i < progresses.length; i++) {
       int remainingProgress = 100 - progresses[i];
       int remainingDays = (int) Math.ceil((double) remainingProgress / speeds[i]);
+      queue.offer(remainingDays); // 남은 날짜를 큐에 추가
+    }
 
-      // 현재 필요한 최대 작업일 수보다 더 많이 걸린다면
-      if (remainingDays > days) {
-        days = remainingDays;
-        // 이전까지의 배포 기능 수를 추가
-        if (i > 0) {
-          answerList.add(cnt);
+    int cnt = 0; // 같은 날에 배포되는 기능 수를 세는 변수
+    int prevDays = 0; // 이전 날의 남은 날짜를 저장하는 변수
+
+    while (!queue.isEmpty()) {
+      int currentDays = queue.poll(); // 큐에서 남은 날짜를 가져옴
+
+      if (currentDays > prevDays) { // 이전 기능보다 더 많은 날이 필요한 경우
+        if (prevDays > 0) {
+          answerList.add(cnt); // 지금까지 배포된 기능 수를 추가
         }
-        cnt = 1; // 현재 배포 기능 수를 1로 초기화
+        cnt = 1; // 현재 날에 배포된 기능 수를 1로 초기화
+        prevDays = currentDays; // 이전 날을 업데이트
       } else {
-        cnt++; // 현재 배포 기능 수를 1 증가
+        cnt++; // 같은 날에 배포된 기능 수를 1 증가
       }
     }
 

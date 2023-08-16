8b24d18117d313eee0ed60ab152b588fa5e10e7f
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-08-16 21:10:46 +0900

    [level 1] Title: 평균 구하기, Time: 0.74 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        int sum = Arrays.stream(arr).sum();
+        answer = (double) sum / arr.length;
+        return answer;
+    }
+}

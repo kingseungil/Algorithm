17bff72cfa105c790856f822216806ec43f05bde
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-08-18 15:06:03 +0900

    [level 1] Title: 시저 암호, Time: 2.62 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,16 +1,17 @@
 class Solution {
-     public String solution(String s,int n){
-        // a(97),A(64),z(122),Z(90)
-        char[] ss = s.toCharArray();
-        char FirstC; // a인지 A인지
-        for (int i = 0; i < s.length(); i++) {
-            if (ss[i] != ' '){
-                // 삼항연산자로 코드간결화
-                FirstC = Character.isUpperCase(ss[i]) ? 'A' : 'a';
-                // 알파벳수가 26개니까 26개안에서만 순환하게 해줌
-                ss[i] = (char)(FirstC + (ss[i]+n-FirstC) % 26);
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+        for (Character c : s.toCharArray()) {
+            if (!Character.isAlphabetic(c)) {
+                sb.append(c);
+                continue;
             }
+            
+            int offset = Character.isUpperCase(c) ? 'A' : 'a';
+            int position = c - offset;
+            position = (position + n) % 26;
+            sb.append((char)(position + offset));
         }
-        return new String(ss);
+        return sb.toString();
     }
 }

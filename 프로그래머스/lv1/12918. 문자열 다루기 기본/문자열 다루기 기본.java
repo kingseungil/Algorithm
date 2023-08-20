cdcaf053523a807181833c06753ee0e00c5db95d
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-08-20 18:59:41 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.11 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean solution(String s) {
-        int stringLength = s.length();
-        if (stringLength != 4 && stringLength != 6) {
-            return false;
-        }
-        return s.matches("[0-9]*");
+        // int stringLength = s.length();
+        // if (stringLength != 4 && stringLength != 6) {
+        //     return false;
+        // }
+        return s.matches("[0-9]{4}|[0-9]{6}");
     }
 }

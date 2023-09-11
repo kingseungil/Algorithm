b691e9ceff1f0e46628bca01bcf8b78a57ddf9cd
김승일 <109774037+kingseungil@users.noreply.github.com>
2023-09-11 12:15:08 +0900

    [level 1] Title: 모의고사, Time: 2.49 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -42,15 +42,13 @@ class Solution {
         
         // 가장 많이 맞춘 사람 찾기 (value 값이 가장 큰 key 값 찾기)
         // value값이 같은 경우 여러명이므로 모두 찾기
-        int max = Math.max(Math.max(firstCount, secondCount), thirdCount);
-        if (max == firstCount) {
-            result.add(1);
-        }
-        if (max == secondCount) {
-            result.add(2);
-        }
-        if (max == thirdCount) {
-            result.add(3);
+        int[] counts = {firstCount, secondCount, thirdCount};
+        int max = Arrays.stream(counts).max().getAsInt();
+
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] == max) {
+                result.add(i + 1);
+            }
         }
 
         // 가장 많이 맞춘 사람이 여럿일 경우 오름차순 정렬

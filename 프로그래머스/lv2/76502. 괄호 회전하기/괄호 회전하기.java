3227b62f24dfcff6146ccf6cf20ca7a978323cb9
김승일 <109774037+kingseungil@users.noreply.github.com>
2023-09-13 20:44:31 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.72 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -18,16 +18,14 @@ class Solution {
     private static boolean check(String s) {
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
-            if (c == '[' || c == '(' || c == '{') {
-                stack.add(c);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                char top = stack.pop();
-                if ((c == ']' && top != '[') || (c == ')' && top != '(') || (c == '}'
-                  && top != '{')) {
-                    return false;
+            switch (c) {
+                case '(' -> stack.push(')');
+                case '[' -> stack.push(']');
+                case '{' -> stack.push('}');
+                case ')', ']', '}' -> {
+                    if (stack.isEmpty() || stack.pop() != c) {
+                        return false;
+                    }
                 }
             }
         }

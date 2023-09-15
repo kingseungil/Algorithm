c57f3334c5b6cd407dcb500cb23ec57898ba57df
김승일 <109774037+kingseungil@users.noreply.github.com>
2023-09-15 12:02:03 +0900

    [level 2] Title: 주식가격, Time: 22.10 ms, Memory: 66.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -3,20 +3,13 @@ import java.util.Stack;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Stack<Integer> stack = new Stack<>();
-
         for (int i = 0; i < prices.length; i++) {
-            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                int index = stack.pop();
-                answer[index] = i - index; // 유지된 시간
+            for (int j = i + 1; j < prices.length; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j]) {
+                    break;
+                }
             }
-            stack.push(i);
-        }
-
-        // 스택에 남아있는 값들은 끝까지 가격이 떨어지지 않은 값들
-        while (!stack.isEmpty()) {
-            int index = stack.pop();
-            answer[index] = prices.length - index - 1;
         }
         return answer;
     }

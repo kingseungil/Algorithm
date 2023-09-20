a5e8e166f019f8f32ec8abccff8b03748261a265
김승일 <109774037+kingseungil@users.noreply.github.com>
2023-09-20 23:13:24 +0900

    [level 2] Title: H-Index, Time: 0.34 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -4,14 +4,16 @@ class Solution {
     public int solution(int[] citations) {
         Arrays.sort(citations);
 
-        int max = 0;
-        for (int i = citations.length - 1; i > -1; i--) {
-            int min = Math.min(citations[i], citations.length - i);
-            if (max < min) {
-                max = min;
+        for (int h = citations.length; h >= 1; h--) {
+            if (isValid(citations, h)) {
+                return h;
             }
         }
+        return 0; // h-index가 0인 경우
+    }
 
-        return max;
+    private static boolean isValid(int[] citations, int h) {
+        int idx = citations.length - h; // h번 이상 인용된 논문의 개수
+        return citations[idx] >= h; // h번 이상 인용된 논문의 개수가 h편 이상이면 true
     }
 }

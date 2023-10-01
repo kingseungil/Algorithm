a11cc18e1e994cc0e7bfcd35ca39f11a4bfc838c
김승일 <109774037+kingseungil@users.noreply.github.com>
2023-10-01 15:56:22 +0900

    [level 1] Title: 하샤드 수, Time: 1.16 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -1,11 +1,9 @@
 class Solution {
     public boolean solution(int x) {
-        int sum = 0;
-        int temp = x;
-        while (temp != 0) {
-            sum += temp % 10;
-            temp /= 10;
-        }
+        int sum = String.valueOf(x)
+                        .chars()
+                        .map(Character::getNumericValue)
+                        .sum();
         return x % sum == 0;
     }
 }

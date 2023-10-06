6c6044c4df7c24c7be2eaa74acfc8de44bc4022a
김승일 <109774037+kingseungil@users.noreply.github.com>
2023-10-06 15:58:33 +0900

    [level 2] Title: 가장 큰 수, Time: 3.32 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -3,13 +3,22 @@ import java.util.stream.Collectors;
 
 class Solution {
     public String solution(int[] numbers) {
-        String strNumbers = Arrays.stream(numbers)
-                                  .mapToObj(String::valueOf)
-                                  .sorted((o1, o2) -> (o2 + o1).compareTo(o1 + o2))
-                                  .collect(Collectors.joining());
-        if (strNumbers.startsWith("0")) {
+        String[] strNumbers = Arrays.stream(numbers)
+                                    .mapToObj(String::valueOf)
+                                    .toArray(String[]::new);
+
+        // 두 문자열을 합쳐서 더 큰 수가 되는 순서로 정렬
+        Arrays.sort(strNumbers, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+
+        if (strNumbers[0].equals("0")) {
             return "0";
         }
-        return strNumbers;
+
+        StringBuilder sb = new StringBuilder();
+        for (String strNumber : strNumbers) {
+            sb.append(strNumber);
+        }
+
+        return sb.toString();
     }
 }

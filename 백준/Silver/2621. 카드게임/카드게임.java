3d76e33bd2966b34a53848e6f5b8ebf223c52e25
Kingseungil <109774037+kingseungil@users.noreply.github.com>
2024-03-09 01:39:02 +0900

    [Silver III] Title: 카드게임, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/2621. 카드게임/카드게임.java
+++ 백준/Silver/2621. 카드게임/카드게임.java
@@ -1,139 +1,200 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/**
- * 2621 카드게임
- */
 public class Main {
-    static List<Card> cards = new ArrayList<>();
-    static Map<String, Integer> colorMap = new HashMap<>();
-    static Map<Integer, Integer> numMap = new HashMap<>();
+
+    static Card[] cards = new Card[5];
+    static int[] colorCount = new int[4]; // R, B, Y, G
+    static int[] numberCount = new int[9];
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        FastReader rd = new FastReader();
         for (int i = 0; i < 5; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            cards.add(new Card(st.nextToken(), Integer.parseInt(st.nextToken())));
+            char color = rd.next().charAt(0);
+            int number = rd.nextInt();
+            cards[i] = new Card(color, number);
         }
 
-        // 숫자 기준으로 정렬
-        Collections.sort(cards);
-
-        // 개수 세기
         for (Card card : cards) {
-            colorMap.put(card.color, colorMap.getOrDefault(card.color, 0) + 1);
-            numMap.put(card.num, numMap.getOrDefault(card.num, 0) + 1);
+            switch (card.color) {
+                case 'R':
+                    colorCount[0]++;
+                    break;
+                case 'B':
+                    colorCount[1]++;
+                    break;
+                case 'Y':
+                    colorCount[2]++;
+                    break;
+                case 'G':
+                    colorCount[3]++;
+                    break;
+            }
+            numberCount[card.number - 1]++;
+        }
+        Arrays.sort(cards);
+
+        int max = Integer.MIN_VALUE;
+
+        // 1. 모두 같은 색이면서 숫자가 연속적일 때
+        if (isSameColor() && isSequence()) {
+            max = cards[4].number + 900;
         }
 
-        int score = 0;
-        int maxNum = cards.get(4).num;
-        int[] sameTwo = new int[2];
+        // 2. 4장의 숫자가 같은 경우
+        for (int i = 0; i < numberCount.length; i++) {
+            if (numberCount[i] == 4) {
+                max = Math.max(max, (i + 1) + 800);
+                break;
+            }
+        }
 
-        // 1. 모두 같은색 && 연속된 수
-        if (colorMap.containsValue(5) && isSequence()) {
-            score = maxNum + 900;
+        // 3. 3장의 숫자가 같고 나머지 2장도 숫자가 같은 경우
+        int three = 0;
+        int two = 0;
+        for (int i = 0; i < numberCount.length; i++) {
+            if (numberCount[i] == 3) {
+                three = i + 1;
+            }
+            if (numberCount[i] == 2) {
+                two = i + 1;
+            }
         }
-        // 2. 4장의 수가 같을 때
-        else if (numMap.containsValue(4)) {
-            score = getKeyByValue(4) + 800;
+        if (three != 0 && two != 0) {
+            max = Math.max(max, three * 10 + two + 700);
         }
-        // 3. 3장의 수가 같고, 나머지 2장의 수가 같을 때
-        else if (numMap.containsValue(3) && numMap.containsValue(2)) {
-            score = getKeyByValue(3) * 10 + getKeyByValue(2) + 700;
-        }
+
         // 4. 5장이 모두 같은 색
-        else if (colorMap.containsValue(5)) {
-            score = maxNum + 600;
-        }
-        // 5. 5장의 수가 연속적일 때
-        else if (isSequence()) {
-            score = maxNum + 500;
-        }
-        // 6. 3장의 수가 같을 때
-        else if (numMap.containsValue(3)) {
-            score = getKeyByValue(3) + 400;
-        }
-        // 7. 2장의 수가 같고, 또 다른 2장의 수가 같을 때
-        else if (isSameTwo(sameTwo)) {
-            score = sameTwo[0] * 10 + sameTwo[1] + 300;
-        }
-        // 8. 2장의 수가 같을 때
-        else if (numMap.containsValue(2)) {
-            score = getKeyByValue(2) + 200;
-        }
-        // 9. 나머지
-        else {
-            score = maxNum + 100;
+        if (isSameColor()) {
+            max = Math.max(max, cards[4].number + 600);
         }
 
-        System.out.println(score);
-    }
+        // 5. 5장의 숫자가 연속적일 때
+        if (isSequence()) {
+            max = Math.max(max, cards[4].number + 500);
+        }
 
-    private static boolean isSameTwo(int[] sameTwo) {
-        int num1 = 0;
-        int num2 = 0;
-        for (Entry<Integer, Integer> entry : numMap.entrySet()) {
-            if (entry.getValue() == 2) {
-                if (num1 == 0) {
-                    num1 = entry.getKey();
+        // 6. 3장의 숫자가 같을 때
+        for (int i = 0; i < numberCount.length; i++) {
+            if (numberCount[i] == 3) {
+                max = Math.max(max, (i + 1) + 400);
+                break;
+            }
+        }
+
+        // 7. 카드 5장 중 2장의 숫자가 같고 또 다른 2장의 숫자가 같을 때
+        int first = 0;
+        int second = 0;
+        for (int i = 0; i < numberCount.length; i++) {
+            if (numberCount[i] == 2) {
+                if (first == 0) {
+                    first = i + 1;
                 } else {
-                    num2 = entry.getKey();
+                    second = i + 1;
                 }
             }
         }
 
-        if (num1 <= num2) {
-            sameTwo[0] = num2;
-            sameTwo[1] = num1;
-        } else {
-            sameTwo[0] = num1;
-            sameTwo[1] = num2;
+        if (first != 0 && second != 0) {
+            max = Math.max(max, second * 10 + first + 300);
         }
-        return num1 != 0 && num2 != 0;
-    }
 
-    private static int getKeyByValue(int value) {
-        int num = 0;
-        for (Entry<Integer, Integer> entry : numMap.entrySet()) {
-            if (entry.getValue() == value) {
-                num = entry.getKey();
+        // 8. 2장의 숫자가 같을 때
+        for (int i = 0; i < numberCount.length; i++) {
+            if (numberCount[i] == 2) {
+                max = Math.max(max, (i + 1) + 200);
                 break;
             }
         }
-        return num;
+
+        // 9. 나머지
+        max = Math.max(max, cards[4].number + 100);
+
+        System.out.println(max);
+    }
+
+
+    private static boolean isSameColor() {
+        return Arrays.stream(colorCount).anyMatch(count -> count == 5);
     }
 
     private static boolean isSequence() {
         for (int i = 0; i < 4; i++) {
-            if (cards.get(i).num + 1 != cards.get(i + 1).num) {
+            if (cards[i].number + 1 != cards[i + 1].number) {
                 return false;
             }
         }
         return true;
     }
 
-    static class Card implements Comparable<Card> {
-        String color;
-        int num;
 
-        Card(String color, int num) {
+    static class Card implements Comparable<Card> {
+
+        char color;
+        int number;
+
+        public Card(char color, int number) {
             this.color = color;
-            this.num = num;
+            this.number = number;
+        }
+
+        @Override
+        public String toString() {
+            return "Card{" +
+                    "color=" + color +
+                    ", number=" + number +
+                    '}';
         }
 
         @Override
         public int compareTo(Card o) {
-            return Integer.compare(num, o.num);
+            return this.number - o.number;
+        }
+    }
+
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
         }
     }
 }
